ca8b09e739686d64e7d9f37811f1639f3ebeeb32
reydon <dhruvarora@delhinerds.com>
2018-11-08 18:34:14 +0530

    changes

--- registration/admin.py
+++ registration/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
 from .models import Registration, PaymentTypes
-from import_export.admin import ImportExportActionModelAdmin
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
 
-class RegisterAdmin(ImportExportActionModelAdmin):
+class RegistrationResource(resources.ModelResource):
+    class Meta:
+        model = Registration
+        fields = ("date_added", "first_name", "middle_name", "last_name", "email", "mobile", "gstin", "address1", "address2", "city", "state", "pincode", "purpose_of_payment__name", "txn_status", "txnid", "txnid_pg","txn_methid","amount", "igst","sgst","cgst")
+
+class RegisterAdmin(ImportExportModelAdmin):
+    resource_class = RegistrationResource
     list_display = ["first_name", "middle_name", "last_name", "email", "id", "txnid", "state", "ipa_enrollment_number", "gstin", "mobile", "purpose_of_payment","cgst", "sgst", "igst"]
     search_fields = ["first_name", "last_name", "email", "mobile", "ipa_enrollment_number", "gstin", "purpose_of_payment", "city", "state"]
     list_filter = ["purpose_of_payment", "state"]
